75792fb7bfa128d90b001e71071d7cd6ae08c43a
Reinhard Simon <rsimon64@gmail.com>
2018-04-20 15:16:45 -0500

    init

---
+++ R/add_e_acc.R
@@ -0,0 +1,43 @@
+add_a_acc <- function() {
+  rstudioapi::insertText(text = "&#0225;")
+}
+
+add_e_acc <- function() {
+  rstudioapi::insertText(text = "&#0233;")
+}
+
+add_i_acc <- function() {
+  rstudioapi::insertText(text = "&#0237;")
+}
+
+add_o_acc <- function() {
+  rstudioapi::insertText(text = "&#0243;")
+}
+
+add_u_acc <- function() {
+  rstudioapi::insertText(text = "&#0250;")
+}
+
+add_A_acc <- function() {
+  rstudioapi::insertText(text = "&#0193;")
+}
+
+add_E_acc <- function() {
+  rstudioapi::insertText(text = "&#0201;")
+}
+
+add_I_acc <- function() {
+  rstudioapi::insertText(text = "&#0205;")
+}
+
+add_O_acc <- function() {
+  rstudioapi::insertText(text = "&#0211;")
+}
+
+add_U_acc <- function() {
+  rstudioapi::insertText(text = "&#0218;")
+}
+
+add_n_tld <- function() {
+  rstudioapi::insertText(text = "&#0241;")
+}
